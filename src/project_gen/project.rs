e287a9e96124eee6dd34507227d6ddfce856523c
René Hansen <rene@hansen-audio.de>
2023-04-06 19:58:38 +0200

    Using tera lib for jinja2 template rendering

--- src/project_gen/project.rs
+++ src/project_gen/project.rs
@@ -1,6 +1,12 @@
+use include_dir::{include_dir, Dir};
 use log::info;
 use serde::{Deserialize, Serialize};
 use std::{collections::HashMap, fs, path::PathBuf};
+use tera::{Context, Tera};
+
+// Compile all files in /templates folder into the binary
+static PROJECT_TEMPLATES: Dir = include_dir!("$CARGO_MANIFEST_DIR/templates");
+static PROJECT_STRUCTURE_TEMPLATE: &str = "project_structure.json";
 
 enum PathType {
     File {
@@ -14,17 +20,17 @@ enum PathType {
 
 #[derive(Debug, Clone)]
 pub struct Project {
-    vars: HashMap<String, String>,
-    templates: HashMap<String, String>,
     out_dir: Option<PathBuf>,
+    domain_name: String,
+    target_name: String,
 }
 
 impl Project {
     pub fn new(domain_name: String, target_name: String, out_dir: Option<PathBuf>) -> Self {
         Self {
-            vars: Self::create_vars(&domain_name.conform(), &target_name.conform()),
-            templates: Self::create_templates(),
             out_dir,
+            domain_name: domain_name.conform(),
+            target_name: target_name.conform(),
         }
     }
 
@@ -34,17 +40,27 @@ impl Project {
             .clone()
             .unwrap_or(PathBuf::from(std::env::current_dir().unwrap().clone()));
 
+        let tera_context = self.new_tera_context();
+
         let folder = self.parse_json_proj_struct();
 
         folder.create_recursively_at(&out_dir, &|path_type| match path_type {
-            PathType::File { path, opt_template } => match opt_template {
-                Some(template) => {
-                    let opt_content = self.templates.get(template);
-                    if let Some(content) = opt_content {
-                        let tmp = content.clone().replace_vars(&self.vars);
-                        fs::write(&path, tmp).expect("Could not write file {path}!");
-                        info!("Created: {path:#?}");
-                    }
+            PathType::File {
+                path,
+                opt_template: opt_template_file,
+            } => match opt_template_file {
+                Some(template_file) => {
+                    let template_content = PROJECT_TEMPLATES
+                        .get_file(template_file)
+                        .unwrap()
+                        .contents_utf8()
+                        .unwrap();
+
+                    let rendered = Tera::one_off(template_content, &tera_context, true)
+                        .expect("Cannot render file {template_file}");
+
+                    fs::write(&path, rendered).expect("Could not write file {path}!");
+                    info!("Created: {path:#?}");
                 }
                 None => (),
             },
@@ -55,51 +71,45 @@ impl Project {
         });
     }
 
+    fn new_tera_context(&self) -> Context {
+        let domain_name = self.domain_name.clone();
+        let target_name = self.target_name.clone();
+        let project_name = format!("{}-{}", domain_name.clone(), target_name.clone());
+
+        let mut config = HashMap::new();
+        config.insert("target_name", target_name);
+        config.insert("domain_name", domain_name);
+        config.insert("project_name", project_name);
+        config.insert("cmake_minimum_version", "3.19.0".to_string());
+
+        let mut context = Context::new();
+        context.insert("with_test_app", &true);
+        context.insert("config", &config);
+        context
+    }
+
     fn parse_json_proj_struct(&self) -> ProjectFolder {
-        let mut json_string = include_str!("res/project_structure.json").to_string();
-        json_string = json_string.replace_vars(&self.vars);
+        let opt_file = PROJECT_TEMPLATES.get_file(PROJECT_STRUCTURE_TEMPLATE);
+        match opt_file {
+            Some(file) => {
+                let opt_path_str = file.path().to_str();
+                if let Some(path_str) = opt_path_str {
+                    let template_content = PROJECT_TEMPLATES
+                        .get_file(path_str)
+                        .unwrap()
+                        .contents_utf8()
+                        .unwrap();
 
-        serde_json::from_str(&json_string).unwrap()
-    }
+                    let rendered = Tera::one_off(template_content, &self.new_tera_context(), true)
+                        .expect("Cannot render file {path_str}");
 
-    fn create_vars(domain_name: &String, target_name: &String) -> HashMap<String, String> {
-        let project_name = format!("{domain_name}-{target_name}");
-        HashMap::from([
-            ("@DOMAIN_NAME@".to_string(), domain_name.clone()),
-            ("@TARGET_NAME@".to_string(), target_name.clone()),
-            ("@PROJECT_NAME@".to_string(), project_name.clone()),
-            ("@CMAKE_MINIMUM_VERSION@".to_string(), "3.19.0".to_string()),
-        ])
-    }
-
-    fn create_templates() -> HashMap<String, String> {
-        let templates = [
-            (
-                "include/print.h".to_string(),
-                include_str!("res/include/print.h.in").to_string(),
-            ),
-            (
-                "source/print_hello_world.cpp".to_string(),
-                include_str!("res/source/print_hello_world.cpp.in").to_string(),
-            ),
-            (
-                "CMakeLists.txt".to_string(),
-                include_str!("res/CMakeLists.txt.in").to_string(),
-            ),
-            (
-                "README.md".to_string(),
-                include_str!("res/README.md.in").to_string(),
-            ),
-            (
-                "external/CMakeLists.txt".to_string(),
-                include_str!("res/external/CMakeLists.txt.in").to_string(),
-            ),
-            (
-                "source/main.cpp".to_string(),
-                include_str!("res/source/main.cpp.in").to_string(),
-            ),
-        ];
-        HashMap::from(templates)
+                    return serde_json::from_str(&rendered).unwrap();
+                } else {
+                    todo!()
+                }
+            }
+            None => todo!(),
+        };
     }
 }
 
@@ -172,21 +182,11 @@ impl ProjectFolder {
 
 trait StringExt {
     fn conform(&self) -> Self;
-    fn replace_vars(self, vars: &HashMap<String, String>) -> Self;
 }
 
 impl StringExt for String {
     fn conform(&self) -> String {
         self.replace(char::is_whitespace, "_").to_lowercase()
-    }
-
-    fn replace_vars(self, vars: &HashMap<String, String>) -> String {
-        let mut string = self.clone();
-        for (key, value) in vars.iter() {
-            string = string.replace(key, value);
-        }
-
-        string
     }
 }
 
@@ -203,5 +203,59 @@ mod tests {
         println!("{proj_struct:#?}");
 
         proj_gen.gen();
+    }
+
+    #[test]
+    fn test_jinja2() {
+        let tera = match Tera::new("templates/**/*") {
+            Ok(t) => t,
+            Err(e) => {
+                println!("Parsing error(s): {}", e);
+                ::std::process::exit(1);
+            }
+        };
+
+        let mut context = Context::new();
+        context.insert("target_name", &"mylib");
+        context.insert("domain_name", &"hao");
+        context.insert("project_name", &"hao-mylib");
+        context.insert("with_test_app", &true);
+        context.insert("cmake_minimum_version", &"3.19.0");
+
+        let mut out_dir = PathBuf::from(std::env::current_dir().unwrap().clone());
+
+        match tera.render("CMakeLists.txt.in", &context) {
+            Ok(s) => {
+                out_dir.push("CMakeLists.txt");
+                fs::write(out_dir, s).expect("Could not write file {path}!");
+            }
+            Err(e) => {
+                println!("Error: {}", e);
+            }
+        };
+    }
+
+    #[test]
+    fn test3() {
+        let mut config = HashMap::new();
+        config.insert("target_name", "mylib");
+        config.insert("domain_name", "hao");
+        config.insert("project_name", "hao-mylib");
+        config.insert("cmake_minimum_version", "3.19.0");
+
+        let mut ctx = Context::new();
+        ctx.insert("config", &config);
+        ctx.insert("with_test_app", &true);
+
+        let path = "project_structure.json";
+        let template = PROJECT_TEMPLATES
+            .get_file(path)
+            .unwrap()
+            .contents_utf8()
+            .unwrap();
+
+        let rendered = Tera::one_off(template, &ctx, true).expect("Cannot load file");
+
+        println!("{}", rendered);
     }
 }
